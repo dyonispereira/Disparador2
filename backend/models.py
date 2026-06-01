1c16c953eb700a7f6552045be3647861e9c6d6d3
Dyonis Pereira <dyonis.nix@gmail.com>
2026-06-01 09:08:05 -0400

    fix: import Text from sqlalchemy in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Float, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
